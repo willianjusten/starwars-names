4e4fcd25d46153bf4f52c4612ad2a2c338d236a1
willianjusten <willianjustenqui@gmail.com>
2015-10-29 22:22:34 -0200

    feat(random): Add ability to get and array of starwars names.
    If you pass a number to the random funcion, you will receive an array with that number of random
    items.
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 var uniqueRandomArray = require('unique-random-array');
 var starWarsNames = require('./starwars-names.json');
+var getRandomItem = uniqueRandomArray(starWarsNames);
 
 module.exports = {
     all: starWarsNames, 
-    random: uniqueRandomArray(starWarsNames)
+    random: random
 };
+
+function random(number) {
+    if (number === undefined) {
+        return getRandomItem();
+    }
+    else {
+        var randomItens = [];
+        for (var i = 0; i < number; i++) {
+            randomItens.push(getRandomItem());
+        }
+        return randomItens;
+    }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -2,26 +2,34 @@ var expect = require('chai').expect;
 var starWars = require('./index');
 
 describe('starwars-names', function() {
-   describe('all', function () {
-       it('should be an array of string', function() {
-           expect(starWars.all).to.satisfy(isArrayOfStrings);
+    describe('all', function () {
+        it('should be an array of string', function() {
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
 
            function isArrayOfStrings(array) {
                 return array.every(function(iten) {
                     return typeof iten === 'string';
                 });
-           }
+            }
        });
 
        it('should contain Luke Skywalker', function() {
             expect(starWars.all).to.include('Luke Skywalker');
        });
-   });
+    });
 
-   describe('random', function () {
-       it('should return a random item from the starWars.all', function() {
+    describe('random', function () {
+        it('should return a random item from the starWars.all', function() {
             var randomItem = starWars.random();
             expect(starWars.all).to.include(randomItem);
-       });
-   });
+        });
+
+        it('should return an array of random itens if passed a number', function() {
+            var randomItens = starWars.random(3);
+            expect(randomItens).to.have.length(3);
+            randomItens.forEach(function(item){
+                expect(starWars.all).to.include(item);
+            });
+        });
+    });
 });
